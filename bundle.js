7349fc9bab5a6d4dcb3b64f706c70857d7f10d4e
Sarang Kartikey <sarangkartikey@sarangkartikey.local>
2021-04-25 14:57:15 +0530

    Updates

---
+++ bundle.js
@@ -0,0 +1,2 @@
+var bundle=(()=>{var __defProp=Object.defineProperty;var __export=(target,all)=>{for(var name in all)__defProp(target,name,{get:all[name],enumerable:true})};var src_exports={};__export(src_exports,{run:()=>run});function run(){console.log("Hello, Github Actions! \u{1F389}")}return src_exports;})();
+//# sourceMappingURL=bundle.js.map
